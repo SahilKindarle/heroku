565a0f048fde8bc5c6f023e9b68585e4278f6662
sahilkindarle <sahilkindarle212@gmail.com>
2018-12-28 20:16:41 +0530

    connection done

--- app.js
+++ app.js
@@ -4,46 +4,27 @@ var path =require('path')
 var logger = require('morgan')
 const bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
-const { Client } = require('pg');
+
 
 const PORT = process.env.PORT || 3000
-const dbname = process.env.DATABASE_URL
-// const DBPATH = process.env.DATABASE_URL || 'postgres://irjmfvxh:EAj3N4DcMuOt8QeZDpJtEufIytTbRN-F@elmer.db.elephantsql.com:5432/irjmfvxh'
+const DBPATH = process.env.DATABASE_URL || 'postgres://irjmfvxh:EAj3N4DcMuOt8QeZDpJtEufIytTbRN-F@elmer.db.elephantsql.com:5432/irjmfvxh'
 
-
-
-
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
 
 //Create Connection
-// const db = mysql.createConnection({
-//     host : 'ec2-107-20-237-78.compute-1.amazonaws.com',
-//     user: 'junhvkycxagcdx',
-//     password: '2c385abfd5643d2c66807224583fc5a01eff0b36c17d5a42f1abec3a9e327132',
-//     database: dbname
-// });
+const db = mysql.createConnection({
+    host : 'db4free.net',
+    user: 'cysta123',
+    password: 'cysta123',
+    database: 'cysta_db'
+});
 
-// db.connect((err) =>{
-//     if(err)
-//     {
-//         throw(err)
-//     }
-//     console.log('connected');
-// });
+db.connect((err) =>{
+    if(err)
+    {
+        throw(err)
+    }
+    console.log('connected');
+});
 const app = express();
 
 
@@ -78,7 +59,7 @@ app.get('/createdb', (req, res) => {
 //Create Table Post name
 app.get('/createtableone', (req,res) => {
     let sql = 'CREATE TABLE tableone(id int AUTO_INCREMENT, name VARCHAR(255), primary key (id))';
-    client.query('CREATE TABLE tableone(id int AUTO_INCREMENT, name VARCHAR(255), primary key (id))', (err, res) =>{
+    db.query(sql, (err, result) =>{
         if(err) throw(err);
         console.log(result);
         res.send('post table created');
